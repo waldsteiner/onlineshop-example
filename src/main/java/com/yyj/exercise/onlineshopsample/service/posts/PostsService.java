ceb3aa0030c862367d99ab1f89658be43d943270
waldsteiner <waldsteiner@gmail.com>
2023-07-02 23:35:20 +0900

    #3 Update 1) 도서 목록 수정 삭제 추가   - 도서 정보 수정 폼에서 삭제 버튼 추가 및 기능 구현

--- src/main/java/com/yyj/exercise/onlineshopsample/service/posts/PostsService.java
+++ src/main/java/com/yyj/exercise/onlineshopsample/service/posts/PostsService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -48,5 +47,13 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
